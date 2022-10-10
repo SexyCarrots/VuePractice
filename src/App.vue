4f567bc0cbe7fa9ea44e45e6d04bdb5fffa03f57
Xinghan Yang <yang-xinghan@outlook.com>
2022-10-10 12:34:34 -0700

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
+<script setup>
+let content = $ref(''), list = $ref([])
+</script>
+
 <template>
-  <router-view></router-view>
+  <p v-if="content">Your input: {{ content }}</p>
+  <p v-else>You don't have any input</p>
+  <input v-model="content">
+  <button @click="list.push(content)">Add</button>
+  <p v-for="item in list">{{ item }}</p>
 </template>
+
+<style scoped>
+input { background: #eee; }
+</style>
---
+++ src/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+import List from './App.vue'
+</script>
+
+<template>
+  <List v-for="i in 3"></list>
+</template>
